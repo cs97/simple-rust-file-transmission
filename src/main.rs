0afdd81daa215548664fd712f8f30e7e4ae81cae
cs97 <chrswr97@gmail.com>
2022-10-18 19:23:16 +0200

    .

--- src/main.rs
+++ src/main.rs
@@ -105,7 +105,7 @@ fn print_usage(prog_name: &str) -> () {
 
 fn doit() -> std::io::Result<()> {
 	let args: Vec<String> = env::args().collect();
-	if args.len() < 2 {
+	if args.len() < 1 {
 		print_usage(&args[0]);
 		return Ok(());
 	}
